25a01c2db975f90ba524b518fb6ef3f4980123a1
kimD0ngjun <chickentasty0112@gmail.com>
2025-05-09 04:19:23 +0900

    feat: 일부 수정

--- src/main/java/CustomExceptionSearcher.java
+++ src/main/java/CustomExceptionSearcher.java
@@ -3,6 +3,6 @@ import org.springframework.stereotype.Component;
 @Component
 public class CustomExceptionSearcher {
     public void search() {
-        System.out.println("테스트용 로깅 출력입니다!!");
+        System.out.println("테스트용 로깅 출력입니다!!!");
     }
 }
