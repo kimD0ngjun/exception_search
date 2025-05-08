fdfc39e3b884d08d928726af941b853c0c99bd12
kimD0ngjun <chickentasty0112@gmail.com>
2025-05-09 03:47:56 +0900

    feat: 테스트용 설정 코드 추가

--- src/main/java/CustomClass.java
+++
@@ -1,8 +0,0 @@
-import org.springframework.stereotype.Component;
-
-@Component
-public class CustomClass {
-    public void test() {
-        System.out.println("테스트용 로그 출력 : 성공하길 빌어용");
-    }
-}
---
+++ src/main/java/ExceptionSearcherAutoConfiguration.java
@@ -0,0 +1,10 @@
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ExceptionSearcherAutoConfiguration {
+    @Bean
+    public CustomExceptionSearcher customExceptionSearcher() {
+        return new CustomExceptionSearcher();
+    }
+}
