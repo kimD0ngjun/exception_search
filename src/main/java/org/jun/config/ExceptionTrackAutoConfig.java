7a9aa18d3b1d5d8156137974d1eb18c8ee13eab6
kimD0ngjun <chickentasty0112@gmail.com>
2025-05-09 23:46:11 +0900

    refactor: 이벤트리스너 빈 등록

--- src/main/java/org/jun/config/ExceptionTrackAutoConfig.java
+++ src/main/java/org/jun/config/ExceptionTrackAutoConfig.java
@@ -1,10 +1,13 @@
 package org.jun.config;
 
+import org.jun.listener.ExceptionListener;
 import org.jun.publish.FilterServletExceptionPublisher;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.annotation.AsyncConfigurer;
+
+import java.net.http.HttpClient;
 
 @Configuration
 public class ExceptionTrackAutoConfig implements AsyncConfigurer {
@@ -16,8 +19,18 @@ public class ExceptionTrackAutoConfig implements AsyncConfigurer {
     }
 
     @Bean
+    public HttpClient httpClient() {
+        return HttpClient.newHttpClient();
+    }
+
+    @Bean
     public CustomExceptionSearcher customExceptionSearcher() {
         return new CustomExceptionSearcher();
+    }
+
+    @Bean
+    public ExceptionListener exceptionListener() {
+        return new ExceptionListener(httpClient());
     }
 
     @Bean
