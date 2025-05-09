b212ec341d84a16c3887b0e89158411b2d5c8f42
kimD0ngjun <chickentasty0112@gmail.com>
2025-05-09 18:22:07 +0900

    feat: 이벤트 퍼블리싱 코드 프레임 추가

--- src/main/java/org/jun/publish/GlobalExceptionPublisher.java
+++ src/main/java/org/jun/publish/GlobalExceptionPublisher.java
@@ -1,5 +1,20 @@
 package org.jun.publish;
 
-//@ControllAdvic
+import org.springframework.context.ApplicationEventPublisher;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
 public class GlobalExceptionPublisher {
+
+    private final ApplicationEventPublisher publisher;
+
+    public GlobalExceptionPublisher(ApplicationEventPublisher publisher) {
+        this.publisher = publisher;
+    }
+
+    @ExceptionHandler(Exception.class)
+    public void handle(Exception e) {
+        System.err.println(e.getMessage());
+    }
 }
