f54fa56fe77b2c5a7698ba145873db467da56133
kimD0ngjun <chickentasty0112@gmail.com>
2025-05-10 17:19:06 +0900

    fix: 로그 수정

--- src/main/java/org/jun/listener/ExceptionListener.java
+++ src/main/java/org/jun/listener/ExceptionListener.java
@@ -1,5 +1,6 @@
 package org.jun.listener;
 
+import org.jun.dto.ExceptionInfoRequest;
 import org.springframework.context.event.EventListener;
 import org.springframework.web.reactive.function.client.WebClient;
 
@@ -12,5 +13,15 @@ public class ExceptionListener {
     }
 
     @EventListener
-    public void listenException() {}
+    public void listenException(ExceptionInfoRequest request) {
+        System.err.println("예외 커스텀 포착");
+        System.out.println("예외명: " + request.getExceptionName() + "\n"
+        + "요청 URI: " + request.getRequestUri() + "\n"
+        + "요청 메세지: " + request.getMessage() + "\n"
+        + "타임스탬프: " + request.getTimestamp() + "\n"
+        + "클래스 출처: " + request.getOriginClass() + "\n"
+        + "메소드 출처: " + request.getOriginMethod() + "\n"
+        + "모듈 출처: " + request.getOriginModule() + "\n"
+        + "스택트레이스: " + request.getStackTrace() + "\n--------------------------------");
+    }
 }
